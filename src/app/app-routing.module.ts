88fdbe5183932cbc1cd6669938776a071f20566b
Ana Paula Lopes Araujo <aplaraujo@gmail.com>
2020-11-19 15:48:36 -0300

    Inclusão de mais componentes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
+import { AppComponent } from './app.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { AboutusComponent } from './aboutus/aboutus.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'dashboard', component: DashboardComponent },
+  {path: 'about', component: AboutusComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
